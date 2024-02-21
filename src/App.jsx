e9691fe9614e01bf56d4b02a53c0e85312b385f0
never47 <guseinovi2002@gmail.com>
2024-02-21 14:53:31 +0100

    feat: logging system

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,27 @@ import {useState} from 'react';
 
 import Player from './components/Player';
 import GameBoard from './components/GameBoard.jsx';
+import Log from './components/Log.jsx';
 
 function App() {
-  const[activePlayer, setActivePlayer] = useState('X');
+  const [gameTurns, setGameTurns] = useState([]);
+  const [activePlayer, setActivePlayer] = useState('X');
 
-  function handleSelectSquare(){
+  function handleSelectSquare(rowIndex, colIndex){
     setActivePlayer((curActivePlayer)=> curActivePlayer === 'X'?'O':'X');
+    setGameTurns(prevTurns =>{
+      let currentPlayer = 'X';
+
+      if(prevTurns.length>0 && prevTurns[0].player==='X'){
+        currentPlayer = 'O';
+      }
+
+      const updatedTurns = [{ square: {row: rowIndex, col: colIndex},
+      player: currentPlayer},
+      ...prevTurns];
+
+      return updatedTurns;
+    });
   }
 
   return (
@@ -17,8 +32,11 @@ function App() {
           <Player initialName="Player1 1" symbol="X" isActive={activePlayer ==='X'}/>
           <Player initialName="Player1 2" symbol="O"isActive={activePlayer ==='O'}/>
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+        <GameBoard onSelectSquare={handleSelectSquare}
+        turns={gameTurns}
+        />
       </div>
+      <Log turns={gameTurns}/>
     </main>
   )
 }
