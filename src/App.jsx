df73760c7d0f97ebd52f00edc71b82a1573e2e35
never47 <guseinovi2002@gmail.com>
2024-02-22 12:51:33 +0100

    feat: shows player name who win

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,11 @@ function deriveActivePlayer(gameTurns){
 }
 
 function App() {
+  const [players, setPlayers] = useState({
+    'X':'Player 1',
+    'O':'Player 2'
+  });
+
   const [gameTurns, setGameTurns] = useState([]);
 
   const activePlayer = deriveActivePlayer(gameTurns);
@@ -47,7 +52,7 @@ function App() {
 
     if(firstSquareSymbol && firstSquareSymbol===secondSquareSymbol
       && firstSquareSymbol===thirdSquareSymbol){
-        winner = firstSquareSymbol;
+        winner = players[firstSquareSymbol];
       }
   }
 
@@ -69,12 +74,31 @@ function App() {
     setGameTurns([]);
   }
 
+  function handlePlayerNameChange(symbol, newName){
+    setPlayers(prevPlayers => {
+      return {
+        ...prevPlayers,
+        [symbol]:newName
+      };
+    });
+  }
+
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player1 1" symbol="X" isActive={activePlayer ==='X'}/>
-          <Player initialName="Player1 2" symbol="O"isActive={activePlayer ==='O'}/>
+          <Player 
+            initialName="Player1 1" 
+            symbol="X" 
+            isActive={activePlayer ==='X'}
+            onChangeName={handlePlayerNameChange}
+          />
+          <Player
+            initialName="Player1 2" 
+            symbol="O"
+            isActive={activePlayer ==='O'}
+            onChangeName={handlePlayerNameChange}
+          />
         </ol>
         {(winner || hasDraw)
          && <GameOver winner={winner}
